068f9705eee0971e185f0062dd5b6e5f9e616abb
SnO2WMaN <me@sno2wman.net>
2026-03-13 11:50:56 +0900

    adding rest

--- ALF/Authors.lean
+++ ALF/Authors.lean
@@ -5,44 +5,113 @@ public structure Author where
   realname: Option String := none
   github: Option String := none
 
+public section
+
 namespace Authors
 
-public def «SnO2WMaN» : Author where
+def «SnO2WMaN» : Author where
   handle := "SnO2WMaN"
   realname := "Mashu Noguchi"
   github := "sno2wman"
 
-public def «iehality» : Author where
+def «iehality» : Author where
   handle := "Palalansoukî"
   realname := "Shogo Saito"
   github := "iehality"
 
-public def «staroperator» : Author where
+def «staroperator» : Author where
   handle := "staroperator"
   realname := "Dexin Zhang"
   github := "staroperator"
 
-public def «caitlindabrera» : Author where
+def «caitlindabrera» : Author where
   handle := "caitlindabrera"
   realname := "Caitlin D'Abrera"
   github := "caitlindabrera"
 
-public def «minchaowu» : Author where
+def «minchaowu» : Author where
   handle := "minchaowu"
   realname := "Minchao Wu"
   github := "minchaowu"
 
-public def «Rajeev Goré» : Author where
+def «Rajeev Goré» : Author where
   handle := "Rajeev Goré"
   realname := "Rajeev Goré"
 
-public def «ianshil» : Author where
+def «ianshil» : Author where
   handle := "ianshil"
   realname := "Ian Shillito"
   github := "ianshil"
 
-public def «Revantha Ramanayake» : Author where
+def «Revantha Ramanayake» : Author where
   handle := "Revantha Ramanayake"
-  realname := "Revantha Ramanayake" 
+  realname := "Revantha Ramanayake"
+
+def «znssong» : Author where
+  handle := "znssong"
+  realname := "Shuhao Song"
+  github := "znssong"
+
+def «YnirPaz» : Author where
+  handle := "YnirPaz"
+  realname := "Ynir Paz"
+  github := "YnirPaz"
+
+def «u5943321» : Author where
+  handle := "u5943321"
+  realname := "Yiming Xu"
+  github := "u5943321"
+
+def «Michael Norrish» : Author where
+  handle := "Michael Norrish"
+  realname := "Michael Norrish"
+
+def «Marco Maggesi» : Author where
+  handle := "Marco Maggesi"
+  realname := "Marco Maggesi"
+
+def «Cosimo Perini Brogi» : Author where
+  handle := "Cosimo Perini Brogi"
+  realname := "Cosimo Perini Brogi"
+
+def «ruplet» : Author where
+  handle := "ruplet"
+  realname := "Paweł Balawender"
+  github := "ruplet"
+
+def «Jesse Michael Han» : Author where
+  handle := "Jesse Michael Han"
+  realname := "Jesse Michael Han"
+
+def «Floris van Doorn» : Author where
+  handle := "Floris van Doorn"
+  realname := "Floris van Doorn"
+
+def «Janis Bailitis» : Author where
+  handle := "Janis Bailitis"
+  realname := "Janis Bailitis"
+
+def «Dominik Kirst» : Author where
+  handle := "Dominik Kirst"
+  realname := "Dominik Kirst"
+
+def «Yannick Forster» : Author where
+  handle := "Yannick Forster"
+  realname := "Yannick Forster"
+
+def «bbentzen» : Author where
+  handle := "bbentzen"
+  realname := "Bruno Bentzen"
+  github := "bbentzen"
+
+def «Huayu Guo» : Author where
+  handle := "Huayu Guo"
+  realname := "Huayu Guo"
+
+def «Dongheng Chen» : Author where
+  handle := "Dongheng Chen"
+  realname := "Dongheng Chen"
 
 end Authors
+
+end
--- ALF/Bibliography.lean
+++ ALF/Bibliography.lean
@@ -20,7 +20,7 @@ public def TheoremProver.slug : TheoremProver → String
   | .Agda => "agda"
   | .Isabelle => "isabelle"
   | .HOL => "hol"
-  | .HOL_Light => "hol_light"
+  | .HOL_Light => "hol-light"
 
 public def TheoremProver.name : TheoremProver → String
   | .Lean4 => "Lean 4"
@@ -29,7 +29,7 @@ public def TheoremProver.name : TheoremProver → String
   | .Agda => "Agda"
   | .Isabelle => "Isabelle"
   | .HOL => "HOL"
-  | .HOL_Light => "HOL-light"
+  | .HOL_Light => "HOL Light"
 
 
 public inductive Tag where
@@ -117,6 +117,102 @@ public section
 
 open Authors
 
+def «znssong/SetTheory» : Repository where
+  url := .mk "znssong" "SetTheory"
+  tp := [.Lean4]
+  authors := [«znssong»]
+  tags := [.set_theory]
+  description := "Formalization of Kunen's inconsistency theorem."
+
+def «staroperator/mathematical-logic-in-lean» : Repository where
+  url := .mk "staroperator" "mathematical-logic-in-lean"
+  tp := [.Lean4]
+  authors := [«staroperator»]
+  tags := [
+    .arithmetic,
+    .incompleteness_theorem,
+    .first_order_logic,
+    .higher_order_logic
+  ]
+  description := "
+    - Gödel's completeness theorem
+    - Representation theorem in Robinson arithmetic
+    - Categoricity of second-order arithmetic and real numbers
+    - Quasi-categoricity of second-order arithmetic and real numbers
+    "
+
+def «YnirPaz/PCF-Theory» : Repository where
+  url := .mk "YnirPaz" "PCF-Theory"
+  tp := [.Lean4]
+  authors := [«YnirPaz»]
+  tags := [.set_theory]
+  description := "A formalization of PCF theory in Lean."
+
+def «caitlindabrera/Sahlqvist» : Repository where
+  url := .mk "caitlindabrera" "Sahlqvist"
+  tp := [.Coq]
+  authors := [«caitlindabrera»]
+  tags := [.modal_logic]
+  description := "Formalization of Sahlqvist's theorem in Coq."
+
+def «D'Abrera Goré 2018» : Publication where
+  title := "Verified synthesis of (very simple) Sahlqvist correspondents via Coq"
+  tp := [.Coq]
+  authors := [«caitlindabrera», «Rajeev Goré»]
+  tags := [.modal_logic]
+  year := 2018
+  repositories := [«caitlindabrera/Sahlqvist»]
+
+def «u5943321/Modal-Logic» : Repository where
+  url := .mk "u5943321" "Modal-Logic"
+  tp := [.HOL]
+  authors := [«u5943321»]
+  tags := [.modal_logic]
+  description := "Formalized modal logic based on _Modal Logic_ based on P. Blackburn et al."
+
+def «Xu Norrish 2020» : Publication where
+  title := "Mechanised Modal Model Theory"
+  tp := [.HOL]
+  authors := [«u5943321», «Michael Norrish»]
+  tags := [.modal_logic]
+  year := 2020
+  doi := "10.1007/978-3-030-51074-9"
+  repositories := [«u5943321/Modal-Logic»]
+
+def «jrh13/hol-light/GL» : Repository where
+  url := .mk "jrh13" "hol-light"
+  tp := [.HOL_Light]
+  authors := [«Marco Maggesi», «Cosimo Perini Brogi»]
+  tags := [
+    .modal_logic,
+    .provability_logic
+  ]
+  description := "Modal completeness for the provability logic GL in HOL Light"
+
+def «Maggesi Perini-Brogi 2021» : Publication where
+  title := "A Formal Proof of Modal Completeness for Provability Logic"
+  tp := [.HOL_Light]
+  authors := [«Marco Maggesi», «Cosimo Perini Brogi»]
+  tags := [
+    .modal_logic,
+    .provability_logic
+  ]
+  year := 2021
+  doi := "10.4230/LIPICS.ITP.2021.26"
+  repositories := [«jrh13/hol-light/GL»]
+
+def «Maggesi Perini-Brogi 2023» : Publication where
+  title := "Mechanising Gödel-Löb Provability Logic in HOL Light"
+  tp := [.HOL_Light]
+  authors := [«Marco Maggesi», «Cosimo Perini Brogi»]
+  tags := [
+    .modal_logic,
+    .provability_logic
+  ]
+  year := 2023
+  doi := "10.1007/s10817-023-09677-z"
+  repositories := [«jrh13/hol-light/GL»]
+
 def «FormalizedFormalLogic/Foundation» : Repository where
   url := .mk "FormalizedFormalLogic" "Foundation"
   tp := [.Lean4]
@@ -145,6 +241,66 @@ def «FormalizedFormalLogic/Foundation» : Repository where
         - Solovay's arithmetic completeness theorem
     "
 
+def «FormalizedFormalLogic/NonClassicalModalLogic» : Repository where
+  url := .mk "FormalizedFormalLogic" "NonClassicalModalLogic"
+  tp := [.Lean4]
+  authors := [«SnO2WMaN»]
+  tags := [.modal_logic]
+  description := "Lean4 Formalization of Non-classical modal logic: (intuitionistic | constructive) modal logic."
+
+def «ruplet/formalization-of-bounded-arithmetic» : Repository where
+  url := .mk "ruplet" "formalization-of-bounded-arithmetic"
+  tp := [.Lean4]
+  authors := [«ruplet»]
+  tags := [.arithmetic]
+  description := "Formalizing bounded arithmetic and proof complexity and proof extraction."
+
+def «flypitch/flypitch» : Repository where
+  url := .mk "flypitch" "flypitch"
+  tp := [.Lean3]
+  authors := [«Jesse Michael Han», «Floris van Doorn»]
+  tags := []
+  description := "A formal proof of the independence of the continuum hypothesis."
+
+def «Bailitis 2024» : Publication where
+  title := "Löb's Theorem and Provability Predicates in Coq"
+  tp := [.Coq]
+  authors := [«Janis Bailitis»]
+  tags := [.incompleteness_theorem]
+  year := 2024
+  repositories := []
+
+def «Bailitis Kirst Forster 2025» : Publication where
+  title := "Löb's Theorem and Provability Predicates in Rocq"
+  tp := [.Coq]
+  authors := [«Janis Bailitis», «Dominik Kirst», «Yannick Forster»]
+  tags := [.incompleteness_theorem]
+  year := 2025
+  repositories := []
+
+def «bbentzen/mpl» : Repository where
+  url := .mk "bbentzen" "mpl"
+  tp := [.Lean3]
+  authors := [«bbentzen»]
+  tags := [.modal_logic]
+  description := "A Henkin-style completeness proof for the modal logic S5"
+
+def «bbentzen/ipl» : Repository where
+  url := .mk "bbentzen" "ipl"
+  tp := [.Lean3]
+  authors := [«bbentzen», «Huayu Guo», «Dongheng Chen»]
+  tags := [.propositional_logic]
+  description := "Verified completeness in Henkin-style for intuitionistic propositional logic"
+
+def «Bentzen 2021» : Publication where
+  title := "A Henkin-Style Completeness Proof for the Modal Logic S5"
+  tp := [.Lean3]
+  authors := [«bbentzen»]
+  tags := [.modal_logic]
+  year := 2021
+  doi := "10.1007/978-3-030-89391-0_25"
+  repositories := [«bbentzen/mpl»]
+
 def «minchaowu/ModalTab» : Repository where
   url := .mk "minchaowu" "ModalTab"
   tp := [.Lean3]
@@ -153,7 +309,7 @@ def «minchaowu/ModalTab» : Repository where
     .modal_logic,
     .tableaux
   ]
-  description := "Lean4 formalization for modal logic tableau method."
+  description := "Verified decision procedures for modal logics"
 
 def «Wu Goré 2019» : Publication where
   title := "Verified Decision Procedures for Modal Logics"
@@ -188,7 +344,7 @@ def «Goré Ramanayake Shillito 2021» : Publication where
     .sequent_calculus
   ]
   year := 2021
-  doi := "10.4230/LIPICS.ITP.2021.17"
+  doi := "10.1007/978-3-030-86059-2_18"
   repositories := [«ianshil/CE_GLS»]
 
 end
@@ -198,9 +354,27 @@ end Bibliography
 open Bibliography in
 
 public def bibliography : List Bibliography := [
+  «Bailitis 2024»,
+  «Bailitis Kirst Forster 2025»,
+  «bbentzen/ipl»,
+  «bbentzen/mpl»,
+  «Bentzen 2021»,
+  «caitlindabrera/Sahlqvist»,
+  «D'Abrera Goré 2018»,
+  «flypitch/flypitch»,
   «FormalizedFormalLogic/Foundation»,
-  «minchaowu/ModalTab»,
-  «Wu Goré 2019»,
+  «FormalizedFormalLogic/NonClassicalModalLogic»,
+  «Goré Ramanayake Shillito 2021»,
   «ianshil/CE_GLS»,
-  «Goré Ramanayake Shillito 2021»
+  «jrh13/hol-light/GL»,
+  «Maggesi Perini-Brogi 2021»,
+  «Maggesi Perini-Brogi 2023»,
+  «minchaowu/ModalTab»,
+  «ruplet/formalization-of-bounded-arithmetic»,
+  «staroperator/mathematical-logic-in-lean»,
+  «u5943321/Modal-Logic»,
+  «Wu Goré 2019»,
+  «Xu Norrish 2020»,
+  «YnirPaz/PCF-Theory»,
+  «znssong/SetTheory»,
 ]
